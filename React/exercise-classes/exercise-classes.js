489709426f9117e92e82a85f2af3db18aac518fd
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2023-05-04 22:27:36 +0200

    Setup new project

--- React/exercise-classes/exercise-classes.js
+++ React/exercise-classes/exercise-classes.js
@@ -1,28 +1,55 @@
 class Animal {
-  constructor(species, legs) {
+  constructor(species, legs, name) {
     this.species = species;
     this.legs = legs;
+    this.name = name;
   }
 
   identify() {
-    console.log(`This animal is a ${this.species}, and has ${this.legs} legs.`);
+    console.log(
+      `This animal is a ${this.species}, and has ${this.legs} legs, and his/her name is ${this.name}`
+    );
   }
 }
 
 class Dog extends Animal {
   constructor() {
-    super("dog", 4); // a dog has 4 legs
+    super("dog", 4, "poppie"); // a dog has 4 legs
   }
 
   bark() {
     console.log("Woof!");
   }
+  fetch(item) {
+    console.log(`Fetching ${item}`);
+  }
 }
 
-const human = new Animal("human", 2);
-human.identify();
+class Cat extends Animal {
+  constructor() {
+    super("cat", 4, "skippy");
+  }
+  meow() {
+    console.log("meow meow");
+  }
+  scratch(item) {
+    console.log(`scratching ${item}`);
+  }
+  call() {
+    console.log("cat's don't listen very well, try again.");
+  }
+}
+
+const human = new Animal("human", 2, "mark");
+// human.identify();
 
 const dog = new Dog();
-dog.bark();
-console.log(dog.legs);
-dog.identify();
+const cat = new Cat();
+// dog.bark();
+// console.log(dog.legs);
+// dog.identify();
+// dog.fetch("bal");
+cat.identify();
+cat.scratch("couch");
+cat.meow();
+cat.call();
