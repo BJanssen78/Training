8c6ee93a9161c69f3b835eac66522241d4db6dae
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2023-07-01 20:21:03 +0200

    updated EventList

--- React/fed-final/src/pages/EventsList.jsx
+++ React/fed-final/src/pages/EventsList.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { FetchServer } from "../../functions/fetchServer";
-import { Card, CardBody, CardHeader, Flex, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Text,
+  Spacer,
+  Stack,
+} from "@chakra-ui/react";
 
 export const EventList = () => {
   const [serverData, setServerData] = useState(null);
@@ -13,33 +21,45 @@ export const EventList = () => {
     <>
       <FetchServer onDataFetched={fetchServerData} />
       <Flex flexDir={"column"} justifyContent={"center"} alignItems={"center"}>
-        {serverData && serverData.fetchedEventList.length > 0 ? (
-          serverData.fetchedEventList.map((item) => (
-            <React.Fragment key={item.eventID}>
-              <Card
-                width={"60vw"}
-                height={"10em"}
-                bg={"whiteAlpha.300"}
-                margin={"25px"}
-              >
-                <CardBody>
-                  <CardHeader
-                    color={"blackAlpha.900"}
-                    textDecoration={"underline"}
-                    textTransform={"uppercase"}
-                    padding={"0"}
-                    fontSize={"1.5em"}
-                  >
-                    {item.eventName}
-                  </CardHeader>
-                  <Text>{item.eventShortDescr}</Text>
-                </CardBody>
-              </Card>
-            </React.Fragment>
-          ))
-        ) : (
-          <p>No events found</p>
-        )}
+        <Stack spacing={"25px"}>
+          {serverData && serverData.fetchedEventList.length > 0 ? (
+            serverData.fetchedEventList.map((item) => (
+              <React.Fragment key={item.eventID}>
+                <Card
+                  width={"60vw"}
+                  height={"10em"}
+                  bg={"whiteAlpha.300"}
+                  // margin={"25px"}
+                  gap={"25px"}
+                  rowGap={"25px"}
+                >
+                  <CardBody>
+                    <CardHeader
+                      color={"blackAlpha.900"}
+                      textDecoration={"underline"}
+                      textTransform={"uppercase"}
+                      padding={"0"}
+                      fontSize={"1.5em"}
+                      fontWeight={"bold"}
+                    >
+                      {item.eventName}
+                    </CardHeader>
+                    <Text>{item.eventShortDescr}</Text>
+                    <Text
+                      justifyItems={"flex-end"}
+                      alignItems={"flex-end"}
+                      justifyContent={"flex-end"}
+                    >
+                      Date : {item.eventDate}
+                    </Text>
+                  </CardBody>
+                </Card>
+              </React.Fragment>
+            ))
+          ) : (
+            <p>No events found</p>
+          )}
+        </Stack>
       </Flex>
     </>
   );
